a04154d2e1249673627c2fe855434cbbf1fd29f7
Dmytro <103310889+Dmytro-Hlushchenko@users.noreply.github.com>
2023-05-18 15:24:43 +0300

    add-user-json

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,30 @@
+export default function Profile({username, tag, location, avatar, stats}) { 
+    return (
+    <div className="profile">
+        <div className="description">
+            <img
+            src={avatar}
+            alt="User avatar"
+            className="avatar"
+            />
+                <p className="name">{username}</p>
+                <p className="tag">{tag}</p>
+                <p className="location">{location}</p>
+        </div>
+
+        <ul className="stats">
+            <li>
+            <span className="label">stats</span>
+            <span className="quantity">1000</span>
+            </li>
+            <li>
+            <span className="label">Views</span>
+            <span className="quantity">2000</span>
+            </li>
+            <li>
+            <span className="label">Likes</span>
+            <span className="quantity">3000</span>
+            </li>
+        </ul>
+    </div> );
+}
