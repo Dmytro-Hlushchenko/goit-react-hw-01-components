c65c549ef5e928efd9ada32b837d5ccbf0d9431a
Dmytro <103310889+Dmytro-Hlushchenko@users.noreply.github.com>
2023-09-25 21:58:11 +0300

    final1.0

--- src/components/FriendListItem/FriendListItem.styled.js
+++ src/components/FriendListItem/FriendListItem.styled.js
@@ -3,7 +3,7 @@ import styled from "@emotion/styled";
 export const FriendItem = styled.li`
     width: 140px;
     display: flex;
-    justify-content: space-around;
+    justify-content: space-evenly;
     align-items: center;
     border-radius: 5px;
     margin-bottom: 5px;
@@ -16,6 +16,5 @@ export const Status = styled.span`
     width: 15px;
     height: 15px;
     border-radius: 100%;
-    margin-left: 10px;
     
 `
